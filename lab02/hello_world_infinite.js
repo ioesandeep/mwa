03cc3650b14004cc31ce553aa137da664d22b5f7
Sandeep Giri <sandeep@Sandeeps-MacBook-Pro.local>
2019-05-01 01:46:06 -0500

    infinite event loop

--- lab02/hello_world_infinite.js
+++ lab02/hello_world_infinite.js
@@ -1,6 +1,10 @@
-(async () => {
-    setTimeout(function () {
-        console.log("world");
-    }, 2000);
-    console.log("hello");
-})();
+function doNotExit() {
+    setTimeout(doNotExit, 0);
+}
+
+setTimeout(function () {
+    console.log('world');
+    //never exit
+    doNotExit();
+}, 2000);
+console.log('hello');
