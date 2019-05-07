4d45a1977cc71395dcedfa2303b81b2d66a39a8d
Sandeep Giri <wdydev@gmail.com>
2019-05-07 14:26:33 -0500

    secret message reveal app

---
+++ lab07/secret-message/modules/secret/secret.service.js
@@ -0,0 +1,12 @@
+const {db} = require('../../db');
+const config = require('../../config');
+
+class SecretService {
+    async getSecret() {
+        const data = await db.collection(config.collections.secret).findOne().project({key: 1, message: 1});
+        return data;
+    }
+
+}
+
+module.exports = SecretService;
