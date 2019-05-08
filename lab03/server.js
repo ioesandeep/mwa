ab98faeafbbd7d617503c4aff71707e2a4ae4773
Sandeep Giri <ioesandeep@gmail.com>
2019-05-08 01:06:38 -0500

    test

--- lab03/server.js
+++ lab03/server.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const file = fs.readFileSync(path.join(__dirname, '20MB.zip'));
+    const file = fs.readFileSync(path.join(__dirname, '50MB.zip'));
     res.end(file);
 });
 server.listen(8080, () => console.log('Open http://localhost:8080 in your browser.'));
 server.on('listening', () => {
     console.log("Server started");
-})
+});
