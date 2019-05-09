c2e4154f6f3a9b98511b7e89d77df226f7634468
Sandeep Giri <ioesandeep@gmail.com>
2019-05-08 22:53:49 -0500

    DaaS lab07

---
+++ lab07/daas/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    logFileName: 'access.log',
+    db: {
+        name: "homework01",
+        user: "homework01",
+        pass: "homework01",
+        uri: "mongodb+srv://sagiri:<password>@cluster0-pykct.mongodb.net/test?retryWrites=true"
+    },
+    collections: {
+        secret: "data"
+    }
+};
