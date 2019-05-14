efe8b3875e34100ba0a0e44f5bdd49f33bb1f3ee
Sandeep Giri <wdydev@gmail.com>
2019-05-14 14:54:26 -0500

    lab12 - angular first lab

--- angular/lecture02/src/app/app.component.ts
+++ angular/lecture02/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <p><strong>App Component</strong></p>
+    <p>Current value of counter is {{componentCounterValue}}</p>
+    <counter [counterValue]="countStart" (counterChange)="getCount($event)">
+      <h4 class="count">Counter component 1</h4>
+    </counter>
+    <counter [counterValue]="countStart%3" (counterChange)="getCount($event)">
+      <h4 class="count">Counter component 2</h4>
+    </counter>
+  `
 })
 export class AppComponent {
-  title = 'lecture02';
+  private title: string;
+  private countStart: number;
+  private componentCounterValue: number;
+
+  constructor() {
+    this.title = 'Angular MWA';
+    this.countStart = new Date().getTime() % 10;
+  }
+
+  public getCount(count: number): void {
+    this.componentCounterValue = count;
+  }
 }
